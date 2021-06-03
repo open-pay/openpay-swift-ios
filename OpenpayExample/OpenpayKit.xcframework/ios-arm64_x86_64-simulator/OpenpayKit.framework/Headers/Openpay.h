604040f540daf663a37b8223c8f12a0a29df38f8
Carlos Hernandez Perez <carloshe@qrsof.com>
2021-06-03 12:03:47 -0500

    se agrega xcframework

---
+++ OpenpayExample/OpenpayKit.xcframework/ios-arm64_x86_64-simulator/OpenpayKit.framework/Headers/Openpay.h
@@ -0,0 +1,20 @@
+//
+//  Openpay.h
+//  Openpay
+//
+//  Created by Israel Grijalva Correa on 10/4/16.
+//  Copyright © 2016 Openpay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <WebKit/WebKit.h>
+
+//! Project version number for Openpay.
+FOUNDATION_EXPORT double OpenpayVersionNumber;
+
+//! Project version string for Openpay.
+FOUNDATION_EXPORT const unsigned char OpenpayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Openpay/PublicHeader.h>
+
+
--- OpenpayExample/RootViewController.swift
+++ OpenpayExample/RootViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class RootViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -28,5 +28,52 @@ class RootViewController: UIViewController {
         self.navigationController?.pushViewController(vc, animated: true)
     }
     
+    @IBAction func printSwiftVersion(_ sender: Any) {
+        #if swift(>=5.4)
+            print("Hello, Swift 5.4")
+        #elseif swift(>=5.3)
+            print("Hello, Swift 5.3")
+        #elseif swift(>=5.2)
+            print("Hello, Swift 5.2")
+        #elseif swift(>=5.1)
+            print("Hello, Swift 5.1")
+        #elseif swift(>=5.0)
+            print("Hello, Swift 5.0")
+
+        #elseif swift(>=4.2)
+            print("Hello, Swift 4.2")
+
+        #elseif swift(>=4.1)
+            print("Hello, Swift 4.1")
+
+        #elseif swift(>=4.0)
+            print("Hello, Swift 4.0")
+
+        #elseif swift(>=3.2)
+            print("Hello, Swift 3.2")
+
+        #elseif swift(>=3.0)
+            print("Hello, Swift 3.0")
+
+        #elseif swift(>=2.2)
+            print("Hello, Swift 2.2")
+
+        #elseif swift(>=2.1)
+            print("Hello, Swift 2.1")
+
+        #elseif swift(>=2.0)
+            print("Hello, Swift 2.0")
+
+        #elseif swift(>=1.2)
+            print("Hello, Swift 1.2")
+
+        #elseif swift(>=1.1)
+            print("Hello, Swift 1.1")
+
+        #elseif swift(>=1.0)
+            print("Hello, Swift 1.0")
+
+        #endif
+    }
 }
 
